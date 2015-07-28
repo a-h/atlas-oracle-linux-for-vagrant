4bd4e1a348fcf2c72a2611c3bb0f8d4dc216896e
Adrian Hesketh <adrianhesketh@hushmail.com>
2015-07-28 16:22:31 +0100

    Updated the scripts to remove Ubuntu specific commands.

--- scripts/dep.sh
+++ scripts/dep.sh
@@ -2,8 +2,4 @@
 #
 # Setup the the box. This runs as root
 
-apt-get -y update
-
-apt-get -y install curl
-
 # You can install anything you need here.
--- scripts/vmware.sh
+++ scripts/vmware.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 # Bail if we are not running inside VMWare.
-if [[ `facter virtual` != "vmware" ]]; then
-    exit 0
-fi
 
 # Install the VMWare Tools from a linux ISO.
 

929a57c23758fb7368d3c71acda56d439d3e5fcb
Adrian Hesketh <adrianhesketh@hushmail.com>
2015-08-01 22:01:06 +0100

    Adding virtualbox guest dependencies.

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -2,6 +2,8 @@
 
 yum -y install bzip2
 yum -y install kernel-uek-devel
+yum -y install kernel-uek-devel-3.8.13-55.1.6.el7uek.x86_64
+yum -y install kernel-devel
 mkdir -p /mnt/virtualbox
 mount -o loop /home/vagrant/VBoxGuest*.iso /mnt/virtualbox
 sh /mnt/virtualbox/VBoxLinuxAdditions.run

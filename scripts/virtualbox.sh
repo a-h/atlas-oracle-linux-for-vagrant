20b02a6377d49cf2b08c84178b11564252a575db
Adrian Hesketh <adrianhesketh@hushmail.com>
2015-08-03 09:29:21 +0100

    Updated to add in a reboot after installing VirtualBox dependencies which may be required if the Kernel is rebuilt.

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -4,10 +4,15 @@ yum -y install bzip2
 yum -y install kernel-uek-devel
 yum -y install kernel-uek-devel-3.8.13-55.1.6.el7uek.x86_64
 yum -y install kernel-devel
+# Reboot in case the kernel has been rebuilt.
+reboot
+sleep 60
 mkdir -p /mnt/virtualbox
 mount -o loop /home/vagrant/VBoxGuest*.iso /mnt/virtualbox
 sh /mnt/virtualbox/VBoxLinuxAdditions.run
 ln -s /opt/VBoxGuestAdditions-*/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions
 umount /mnt/virtualbox
-rm -rf /home/vagrant/VBoxGuest*.iso
-/etc/init.d/vboxadd setup
+# Keep the file around in case we need to reinstall for some reason.
+# rm -rf /home/vagrant/VBoxGuest*.iso
+# This does a reinstallation.
+# /etc/init.d/vboxadd setup

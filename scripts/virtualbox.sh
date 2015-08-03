f502f451a44a616f1361f9fdc8107741c90c9b0f
Adrian Hesketh <adrianhesketh@hushmail.com>
2015-08-03 11:21:51 +0100

    Split Virtualbox setup over two files

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-yum -y install bzip2
-yum -y install kernel-uek-devel
-yum -y install kernel-uek-devel-3.8.13-55.1.6.el7uek.x86_64
-yum -y install kernel-devel
-# Reboot in case the kernel has been rebuilt.
-reboot
-sleep 60
 mkdir -p /mnt/virtualbox
 mount -o loop /home/vagrant/VBoxGuest*.iso /mnt/virtualbox
 sh /mnt/virtualbox/VBoxLinuxAdditions.run
